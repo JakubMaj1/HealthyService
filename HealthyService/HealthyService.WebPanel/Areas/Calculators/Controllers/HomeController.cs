7de38fd59075b90364d02ab08c2fc3878b5d96f6
jakub <jakubpiotrmaj@gmail.com>
2020-04-22 00:04:36 +0200

    Add Training Views + Calculators Views and Form

---
+++ HealthyService/HealthyService.WebPanel/Areas/Calculators/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyService.WebPanel.Areas.Calculators.Controllers
+{
+    [Area("Calculators")]
+    public class HomeController : Controller
+    {
+        public IActionResult BMI()
+        {
+            return View();
+        }
+        public IActionResult BMR()
+        {
+            return View();
+        }
+        public IActionResult Lift()
+        {
+            return View();
+        }
+        public IActionResult PrimaryCalculators()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ HealthyService/HealthyService.WebPanel/Areas/Training/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyService.WebPanel.Areas.Training.Controllers
+{   [Area("Training")]
+    public class HomeController : Controller
+    {
+        public IActionResult TrainingFBW()
+        {
+            return View();
+        }
+        public IActionResult TrainingSplit()
+        {
+            return View();
+        }
+        public IActionResult PrimaryTraining()
+        {
+            return View();
+        }
+         public IActionResult Calisthenics()
+        {
+            return View();
+        }
+
+    }
+}
