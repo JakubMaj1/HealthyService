3b51344d606e7bd72cdbce8d6b29ae146809d529
jakub <jakubpiotrmaj@gmail.com>
2020-04-22 21:57:53 +0200

    Edit Dashboard + Add View Measurement

---
+++ HealthyService/HealthyService.WebPanel/Areas/Users/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyService.WebPanel.Areas.Users.Controllers
+{
+    [Area("Users")]
+    public class HomeController : Controller
+    {
+        public IActionResult MeasurementAdd()
+        {
+            return View();
+        }
+    }
+}
